e0b5144ac8e8b7478db4a3d7376f1f3138120cd8
y-asai <ryuta234.you@gmail.com>
2019-04-30 16:16:05 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -172,10 +172,10 @@ class LinebotController < ApplicationController
     logger.debug ( "QUERY> " )
     # query = gets.chomp
     # https://www.rubydoc.info/github/google/google-api-ruby-client/Google%2FApis%2FCustomsearchV1%2FCustomsearchService:list_cses
-    results = searcher.list_cses(word, cx: cse_id, search_type: 'image')
+    results = searcher.list_cses(word, cx: cse_id, search_type: 'image', start: 2)
     items = results.items
     # logger.debug(items)
-    items.map {|item| { title: item.title, link: item.link} }
+    pp items.map {|item| { title: item.title, link: item.link} }
     # logger.debug("res = " + res)
   end
   # function google_image($word) {
