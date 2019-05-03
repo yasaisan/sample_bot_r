32f200bc432fa7906111c64943a482308316c35f
y-asai <ryuta234.you@gmail.com>
2019-05-03 16:06:25 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -149,8 +149,8 @@ class LinebotController < ApplicationController
                         imageUrl: var[:image].thumbnail_link,
                         action: {
                           type: "image",
-                          originalContentUrl: var[:image].thumbnail_link,
-                          previewImageUrl: var[:image].thumbnail_link
+                          originalContentUrl: var[:image].thumbnail_link
+                          # previewImageUrl: var[:image].thumbnail_link
                         }
                       },
                       {
